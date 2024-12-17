11c19cf347c6718734902ac58565f6777c5d8320
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-17 09:38:54 +0100

    feat: prova push

--- src/components/appFooter.jsx
+++ src/components/appFooter.jsx
@@ -2,7 +2,7 @@ import style from "./AppFooter.module.css"
 
 function AppFooter ( ) {
     return (
-   <footer className={style.footer}>
+   <footer className={style.footer} >
     <div className="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad reiciendis blanditiis hic ipsam necessitatibus ut cupiditate, ipsa neque praesentium illum repudiandae dolore placeat dolorem voluptatem consequuntur deserunt ex eos deleniti!</div>
     </footer>
     );
